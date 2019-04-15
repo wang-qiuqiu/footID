97959a6d7e7d3020f42a7d9f0e475cf5085ca062
wang-qiuqiu <jiangjiadongdlut@163.com>
2019-04-15 18:25:24 +0800

    default

--- pickData_display.py
+++ pickData_display.py
@@ -1,12 +1,15 @@
 import os
 import shutil
+from footid1 import *
+from scipy import ndimage
+import scipy
 
 
 # 选择集外图片
 def pick_outlier():
     count = 0
     std_path = 'C:\\Users\\Neo\\Desktop\\source_test'
-    source_path = 'C:\\Users\\Neo\\Desktop\\source_display'
+    source_path = 'C:\\Users\\Neo\\Desktop\\source'
     target_path = 'C:\\Users\\Neo\\Desktop\\display_outlier'
 
     deleteFolder(target_path)
@@ -14,8 +17,9 @@ def pick_outlier():
     std_folder = []
     for folder in os.listdir(std_path):
         std_folder.append(folder)
+
     for folder in os.listdir(source_path):
-        if count == 500:
+        if count == 1000:
             break
         if folder in std_folder:
             continue
@@ -27,6 +31,80 @@ def pick_outlier():
                 count += 1
                 print("拷贝：" + str(count) + " " + pic + "完成")
                 break
+
+
+# 选图(集外)
+def select_outlier():
+    source_path = 'C:\\Users\\Neo\\Desktop\\display_outlier'
+    lis = np.load('display_data_label_npy\\data_label.npy')
+    source = np.load('display_std_similarity\\std_similarity.npy')
+    count = 0
+    with tf.Session() as sess:
+        foot_saver.restore(sess, 'display_checkpoint\\30000.ckpt')
+        for pic in os.listdir(source_path):
+            pic_path = os.path.join(source_path, pic)
+            image = np.array(ndimage.imread(pic_path, flatten=False))
+            image = scipy.misc.imresize(image, size=(64, 32))
+            cv2.imwrite('./pick.jpg', image)
+            img = cv2.imread('./pick.jpg')
+            img_list = []
+            img_list.append(img)
+            img = np.array(img_list)
+            result_folder = ''
+            h_test_predict = sess.run(tf.nn.softmax(y), {h0: img})
+            class_ = tf.argmax(h_test_predict, 1)
+            class_ = sess.run(class_)
+            for i in range(lis.shape[0]):
+                if lis[i, 1] == str(class_[0]):
+                    result_folder = lis[i, 0]
+                    break
+            target = ''
+            for row in range(source.shape[0]):
+                if source[row, 0] == result_folder:
+                    target = source[row, 1]
+            t = float(0.90)
+            if float(target) < t:
+                count += 1
+                print(str(count) + '可选：' + '' + pic)
+            else:
+                os.remove(pic_path)
+
+
+# 选图(集内)
+def select_inner():
+    source_path = 'C:\\Users\\Neo\\Desktop\\display_inner'
+    lis = np.load('display_data_label_npy\\data_label.npy')
+    source = np.load('display_std_similarity\\std_similarity.npy')
+    count = 0
+    with tf.Session() as sess:
+        foot_saver.restore(sess, 'display_checkpoint\\30000.ckpt')
+        for pic in os.listdir(source_path):
+            pic_path = os.path.join(source_path, pic)
+            image = np.array(ndimage.imread(pic_path, flatten=False))
+            image = scipy.misc.imresize(image, size=(64, 32))
+            cv2.imwrite('./pick.jpg', image)
+            img = cv2.imread('./pick.jpg')
+            img_list = []
+            img_list.append(img)
+            img = np.array(img_list)
+            result_folder = ''
+            h_test_predict = sess.run(tf.nn.softmax(y), {h0: img})
+            class_ = tf.argmax(h_test_predict, 1)
+            class_ = sess.run(class_)
+            for i in range(lis.shape[0]):
+                if lis[i, 1] == str(class_[0]):
+                    result_folder = lis[i, 0]
+                    break
+            target = ''
+            for row in range(source.shape[0]):
+                if source[row, 0] == result_folder:
+                    target = source[row, 1]
+            t = float(0.90)
+            if float(target) < t:
+                os.remove(pic_path)
+            else:
+                count += 1
+                print(str(count) + '可选：' + '' + pic)
 
 
 # 清空文件夹
@@ -50,12 +128,12 @@ def pick_inner():
     deleteFolder(target_path)
 
     for folder in os.listdir(pick_path):
-        if count_all == 500:
-            break
+        # if count_all == 500:
+        #     break
         folder_path = os.path.join(pick_path, folder)
         count = 0
         for pic in os.listdir(folder_path):
-            if count == 2:
+            if count == 3:
                 break
             else:
                 pic_path = os.path.join(folder_path, pic)
@@ -68,6 +146,8 @@ def pick_inner():
 def main():
     # pick_outlier()
     pick_inner()
+    # select_outlier()
+    # select_inner()
 
 
 if __name__ == '__main__':
